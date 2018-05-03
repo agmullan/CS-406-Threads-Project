f40be84445d951c1c37bd2b85affb1ffb5d5afc7
FrancineDennehy <frankie.dennehy@gmail.com>
2018-05-02 21:07:27 -0400

    Update thread.c

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -215,7 +215,7 @@ thread_create (const char *name, int priority,
   /* add lock to lock_list if one is given */
 if(aux == NULL){
   if(is_lock(aux)){
-      list_push_front(&lock_list, &aux->elem);
+      list_push_front(&lock_list, aux->elem);
       priority_donate(aux);
    }//end if 
 }	
